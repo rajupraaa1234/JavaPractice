a748631a8eb170566512e7d40c64ae466ac4716b
Raju kumar <rajupraaa1234@gmail.com>
2021-07-18 11:27:48 -0700

    Practice While,do while and break

---
+++ JavaPractice/src/Pratice/WhileLoopPractice.java
@@ -0,0 +1,29 @@
+package Pratice;
+
+public class WhileLoopPractice {
+
+	public static void main(String[] args) {
+		    int i=1;  
+		    while(i<=10){  
+		        System.out.println(i);  
+		    i++;  
+		    }  
+		    
+		    int j=1;    
+		    do{    
+		        System.out.println(j);    
+		    j++;    
+		    }while(j<=10);    
+		    
+		    int k=1;
+		    while(k<=15) {
+		    	if(k==8) {
+		    		break;
+		    	}
+		    	System.out.println(k+ " ");
+		    	k++;
+		    }
+
+	}
+
+}
