af8a80e5d95ee7b33ad0b98b19e1fab289c2b191
Raju kumar <rajupraaa1234@gmail.com>
2021-07-18 11:13:20 -0700

    If Else else if nested if else condition

--- JavaPractice/src/Pratice/PracticeTest.java
+++ JavaPractice/src/Pratice/PracticeTest.java
@@ -5,7 +5,51 @@ public class PracticeTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hii Raju");
-
+		
+		    int age=20;    
+		    if(age>18){  
+		        System.out.print("Age is greater than 18");  
+		    }  
+            
+		    int number=13;    
+		    if(number%2==0){  
+		        System.out.println("even number");  
+		    }else{  
+		        System.out.println("odd number");  
+		    }  
+		    
+		    
+		    int marks=65;  
+		      
+		    if(marks<50){  
+		        System.out.println("fail");  
+		    }  
+		    else if(marks>=50 && marks<60){  
+		        System.out.println("D grade");  
+		    }  
+		    else if(marks>=60 && marks<70){  
+		        System.out.println("C grade");  
+		    }  
+		    else if(marks>=70 && marks<80){  
+		        System.out.println("B grade");  
+		    }  
+		    else if(marks>=80 && marks<90){  
+		        System.out.println("A grade");  
+		    }else if(marks>=90 && marks<100){  
+		        System.out.println("A+ grade");  
+		    }else{  
+		        System.out.println("Invalid!");  
+		    }  
+		    
+		    
+		    int age1=20;  
+		    int weight=80;    
+		 
+		    if(age1>=18){    
+		        if(weight>50){  
+		            System.out.println("You are eligible to donate blood");  
+		        }    
+		    }    
 	}
 
 }
